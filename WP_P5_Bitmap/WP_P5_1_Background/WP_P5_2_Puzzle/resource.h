57d6ff0ec1f23b8ceb578efd58c8a893eace5b23
triglan <121035322+triglan@users.noreply.github.com>
2023-05-10 13:14:44 +0900

    5-2

---
+++ WP_P5_Bitmap/WP_P5_1_Background/WP_P5_2_Puzzle/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Resource.rc에서 사용되고 있습니다.
+
+#define IDB_BITMAP1                     101
+#define IDB_BITMAP2                     102
+#define IDR_MENU1                       103
+#define ID_PICTURE_PICTURE              40001
+#define ID_PICTURE_PICTURE40002         40002
+#define ID_PICTURE_PICTURE40003         40003
+#define ID_PICTURE_PICTURE40004         40004
+#define ID_PICTURE_PICTURE40005         40005
+#define ID_DEVIDEPICTURE_PICTURE        40006
+#define ID_DEVIDEPICTURE_PICTURE40007   40007
+#define ID_DEVIDEPICTURE_PICTURE40008   40008
+#define ID_GAME_GAMESTART               40009
+#define ID_GAME_SHOW                    40010
+#define ID_GAME_REVERSE                 40011
+#define ID_GAME_GAME                    40012
+#define ID_PICTURE_PICTURE_1            40013
+#define ID_PICTURE_PICTURE_2            40014
+#define ID_DEVIDEPICTURE_PICTURE_3      40015
+#define ID_DEVIDEPICTURE_PICTURE_4      40016
+#define ID_DEVIDEPICTURE_PICTURE_5      40017
+#define ID_GAME_Quit                    40018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
